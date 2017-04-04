7d60a373ee32116349baa421dd72c98edcae5fbe
afanty <ccdevote@gmail.com>
2017-04-05 04:16:40 +0800

    add sheet

--- src/component/Main.js
+++ src/component/Main.js
@@ -2,7 +2,13 @@ import React from "react";
 import {mixin} from "transdux";
 import AppBar from "material-ui/AppBar";
 import Menu from "./Menu";
-
+import "./Main.css";
+import MobileTearSheet from "./Sheet";
+import {List, ListItem} from "material-ui/List";
+import ContentInbox from "material-ui/svg-icons/content/inbox";
+import ActionGrade from "material-ui/svg-icons/action/grade";
+import Divider from "material-ui/Divider";
+import Avatar from "material-ui/Avatar";
 
 class Main extends React.Component {
 
@@ -12,9 +18,34 @@ class Main extends React.Component {
 
     render() {
         return (
-            <div className="Main">
+            <div className="main">
                 <AppBar onLeftIconButtonTouchTap={this.click.bind(this)} title="xx管理系统"/>
-                <Menu/>
+                <div className="main container">
+                    <div className="main container menus">
+                        <MobileTearSheet>
+                            <List>
+                                <ListItem
+                                    disabled={true}
+                                    leftAvatar={
+                                        <Avatar
+                                            size={30}
+                                        >
+                                            A
+                                        </Avatar>
+                                    }>
+                                </ListItem>
+                            </List>
+                            <Divider/>
+                            <List>
+                                <ListItem primaryText="CM页面" leftIcon={<ContentInbox />}/>
+                                <ListItem primaryText="用户中心" leftIcon={<ActionGrade />}/>
+                            </List>
+                        </MobileTearSheet>
+                    </div>
+                    <div className="main container content">
+                        <pre> 123 456 6</pre>
+                    </div>
+                </div>
             </div>
         )
     }
